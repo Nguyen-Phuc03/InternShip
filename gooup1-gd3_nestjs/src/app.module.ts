12aae8e0f8264d6cb9259e7e5e44d285a4b0b510
Nguyen-Phuc03 <nphuc305072@gmail.com>
2024-08-08 11:04:44 +0700

    module

--- gooup1-gd3_nestjs/src/app.module.ts
+++ gooup1-gd3_nestjs/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Usermodule } from './Modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [Usermodule],
   controllers: [AppController],
   providers: [AppService],
 })
