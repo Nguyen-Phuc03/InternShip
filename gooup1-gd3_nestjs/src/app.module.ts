0331a3916a992fadd685de8e72e5f4075c03b952
Nguyen-Phuc03 <nphuc305072@gmail.com>
2024-08-21 09:17:44 +0700

    okok

--- gooup1-gd3_nestjs/src/app.module.ts
+++ gooup1-gd3_nestjs/src/app.module.ts
@@ -3,16 +3,13 @@ import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
 import { DatabaseModule } from './database/database.module';
-//import { I18nJsonLoader, I18nModule } from 'nestjs-i18n';
-//import { I18nService } from './i18n/i18n.service';
-//import * as path from 'path';
 @Module({
   imports: [
     UserModule,
     DatabaseModule,
     DatabaseModule.forRoot({
       type: 'postgres',
-      url: 'postgres://localhost/Hotel_booking_system',
+      url: 'postgres://localhost:5432/Hotel_booking_system',
     }),
   ],
   providers: [UserService],
